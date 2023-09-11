1b062e3388e4285ae83d2bbf89d46fb57d2ea336
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-09-11 17:40:30 -0300

    Iniciando aplicação spring boot e adicionando página home

---
+++ pi_project/src/main/java/com/pi_engenhariadesoftware/pi_project/PiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pi_engenhariadesoftware.pi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiProjectApplication.class, args);
+	}
+
+}
---
+++ pi_project/src/test/java/com/pi_engenhariadesoftware/pi_project/PiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi_engenhariadesoftware.pi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
