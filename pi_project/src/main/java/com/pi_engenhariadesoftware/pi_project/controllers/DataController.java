82cd126ba27572c36919dd72ec1f78e80396c6e4
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-11-03 14:19:08 -0300

    Página de redirecionamento para confirmação de registro de interesse finalizado

--- pi_project/src/main/java/com/pi_engenhariadesoftware/pi_project/controllers/DataController.java
+++ pi_project/src/main/java/com/pi_engenhariadesoftware/pi_project/controllers/DataController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.pi_engenhariadesoftware.pi_project.entities.Car;
 import com.pi_engenhariadesoftware.pi_project.entities.User;
@@ -53,7 +54,7 @@ public class DataController {
     }
 
     @RequestMapping("/interest")
-    public String interest(@RequestParam("id") Long id, @RequestParam("name") String name, @RequestParam("phone") String phone){
+    public ModelAndView interest(@RequestParam("id") Long id, @RequestParam("name") String name, @RequestParam("phone") String phone){
         
         // Adicionando true para interresse do carro
         Car car = carRepository.getReferenceById(id);
@@ -63,51 +64,8 @@ public class DataController {
         Report report = new Report(car, name, phone);
         reportRepository.save(report);
 
-        return "<!DOCTYPE html>\r\n" + //
-                "<html lang=\"en\">\r\n" + //
-                "<head>\r\n" + //
-                "    <meta charset=\"UTF-8\">\r\n" + //
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
-
-                "    <link rel='icon' href='/img/icon.png'>\r\n" + 
-                "    <title>Interest</title>\r\n" + //
-                "</head>\r\n" + //
-                "<style>\r\n" +
-                "body{\r\n" +
-                    "height: 100%;\r\n" +
-                    "background-color: rgb(28, 28, 28);\r\n" +
-                    "display: flex;\r\n" +
-                    "justify-content: center;\r\n" +
-                    "align-items: center;\r\n" +
-                "}\r\n" +
-                "main{\r\n" +
-                    "display: flex;\r\n" +
-                    "flex-direction: column;\r\n" +
-                    "justify-content: center;\r\n" +
-                    "align-items: center;\r\n" +
-                    "gap: 1rem;\r\n" +
-                    "font-family: sans-serif;\r\n" +
-                    "color: white;\r\n" +
-                "}\r\n" +
-                
-                "button{\r\n" +
-                    "padding: 1rem 2rem;\r\n" +
-                    "border: none;\r\n" +
-                    "background-color: red;\r\n" +
-                    "color: white;\r\n" +
-                    "border-radius: 0.2rem;\r\n" +
-                    "cursor: pointer;\r\n" +
-                    "transition: 0.5s;\r\n" +
-                "}\r\n" +
-            "</style>\r\n" +
-                "<body>\r\n" + //
-                "    <main>\r\n" + //          
-                "       <h1>Interest registered sucessfully!</h1>\r\n" + //
-                "       <a href=\"/\"><button>Back to home</button></a>\r\n" + //                  
-                "    </main>\r\n" + //
-                "</body>\r\n" + //
-                "</html>";
-       
+        ModelAndView modelAndView = new ModelAndView("interest");
+        return modelAndView;
     }
 }
 
